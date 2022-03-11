35a780bf4b6bd74fa733e9aa0b55e94aaa192ac1
Henrique Kubinhetz <rickubinhetz@gmail.com>
2022-03-10 21:42:35 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    all_posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    return render_template("index.html", posts=all_posts)
+
+
+@app.route('/post/<int:postid>')
+def get_post(postid):
+    all_posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    return render_template("post.html", posts=all_posts, postid=postid)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
